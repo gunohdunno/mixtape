8b00697cbf942a53c8f378c126b78a68220aeff7
gunohdunno <kgjung253@gmail.com>
2021-05-16 19:55:05 -0500

    Updated README and created dictionaries for quick song and user validation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # mixtape
-basic python application to parse basic JSON files with music data
+Basic python application to parse music data JSON files and apply any changes given a changes JSON file
+
+## Running the Application
+Run the program through the terminal with two command-line arguments like so:
+```
+python apply_changes.py mixtape.json changes.json
+```
+
+The changes file is a JSON file and has the following attributes for a single change:
+- action: the action to perform (add playlist, remove playlist, add song to a playlist)
+- playlist_id: playlist ID
+- song_id: song ID
+- user_id: user ID
+
+Check changes.json to see an example of the formatting
+
+## Scaling the Application
+Given a large changes.json file, we could create a stream of changes so that we are not loading all the changes that are needed into memory. Since one change is applied at a time, we only need to load the current change into memory and process that change until we reach the end of the file. If memory is not too much of a constraint, we could also load changes in chunks rather than one at time.
+
+Given a large mixtape.json file, we could continue to use the same approach but it would not be as efficient given the current structure of the file. The changes are only applying to playlists but we have access to a list of users and songs that do not change. We only use the user and song lists to validate that a given song or user exists. It may be better to store the users and songs into a database and have mixtape.json just be a list of playlists. That way, we have less to load into memory and we could even use a similar data stream model used in large changes.json files.
--- apply_changes.py
+++ apply_changes.py
@@ -1,61 +1,80 @@
 import sys
 import json
+import collections
 
 def main(argv):
+    global songDict
+    global userDict
+    global playlistDict
     # load json data from mixtape and changes files
-    global mixtape_json
-    global changes_json
+    if len(argv) == 0:
+        raise Exception("mixtape and changes files required")
+    
     with open(argv[0], "r") as f:
-        mixtape_json = json.load(f)
+        mixtapeJSON = json.load(f)
 
     with open(argv[1], "r") as f:
-        changes_json = json.load(f)
+        changesJSON = json.load(f)
+
+    # store into individual dictionaries for quick validation and access times
+    songDict = listToDict(mixtapeJSON["songs"])
+    userDict = listToDict(mixtapeJSON["users"])
+    playlistDict = listToDict(mixtapeJSON["playlists"])
 
     # apply changes to mixtape_json
-    for change in changes_json:
+    for change in changesJSON:
         applyChange(change)
-    
-    # dump data back into output json 
-    with open("output.json", "w") as outfile:
-        json.dump(mixtape_json, outfile)
 
-# check which type of action is being performed on the mixtape and call proper functions accordingly
-def applyChange(change):
-    if change["action"] == "addPlaylist":
-        addPlaylist(change["playlist_id"], change["user_id"], change["song_id"])
-    elif change["action"] == "addSong":
-        addSongToPlaylist(change["playlist_id"], change["song_id"])
-    elif change["action"] == "remove":
-        removePlaylist(change["playlist_id"])
+    # dump data back into output.json 
+    mixtapeJSON["playlists"] = playlistDict.values()
+    with open("output.json", "w") as outfile:
+        json.dump(mixtapeJSON, outfile)
 
 def addPlaylist(playlist_id, user_id, song_id):
     # check for duplicate ids
-    for p in mixtape_json["playlists"]:
-        if p["id"] == playlist_id:
-            return False
-
+    if playlist_id in playlistDict:
+        raise Exception("Playlist ID: " + playlist_id + " does already exists")
+    if user_id not in userDict:
+        raise Exception("user ID: " + user_id + " not found")
+    if song_id not in songDict:
+        raise Exception("song ID: " + song_id + " not found")
+    
     # new playlist is being added
     newPlayList = {}
     newPlayList["id"] = playlist_id
     newPlayList["user_id"] = user_id
     newPlayList["song_ids"] = [song_id]
-    mixtape_json["playlists"].append(newPlayList)
-    return True
+    playlistDict[playlist_id] = newPlayList
 
 def addSongToPlaylist(playlist_id, song_id):
     # find playlist and append new song 
-    for p in mixtape_json["playlists"]:
-        if p["id"] == playlist_id:
-            p["song_ids"].append(song_id)
-            return True
-    return False
+    if playlist_id not in playlistDict:
+        raise Exception("Playlist ID: " + playlist_id + " does not exist")
+    
+    playlistDict[playlist_id]["song_ids"].append(song_id)
 
 def removePlaylist(playlist_id):
-    for p in mixtape_json["playlists"]:
-        if p["id"] == playlist_id:
-            mixtape_json["playlists"].remove(p)
-            return True
-    return False
+    if playlist_id not in playlistDict:
+        raise Exception("Playlist ID: " + playlist_id + " does not exist")
+    
+    del playlistDict[playlist_id]
+
+### Helper functions
+# check which type of action is being performed on the mixtape and call proper functions accordingly
+def applyChange(change):
+    action = change["action"]
+    if action == "addPlaylist":
+        addPlaylist(change["playlist_id"], change["user_id"], change["song_id"])
+    elif action == "addSong":
+        addSongToPlaylist(change["playlist_id"], change["song_id"])
+    elif action == "remove":
+        removePlaylist(change["playlist_id"])
+
+def listToDict(l):
+    d = collections.OrderedDict()
+    for el in l:
+        d[el["id"]] = el
+    return d
 
 if __name__=="__main__":
     main(sys.argv[1:])
